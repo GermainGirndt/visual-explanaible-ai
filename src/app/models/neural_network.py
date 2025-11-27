6168ad65746cb08acedcd85fffe2c4c7a6210378
Germain Girndt <germaingirndt@outlook.com>
2025-11-27 21:57:34 +0100

    Refactor NeuralNetwork initialization: Simplify model loading and remove unnecessary comments for clarity.

--- src/app/models/neural_network.py
+++ src/app/models/neural_network.py
@@ -9,44 +9,19 @@ from app.models.predictions import Predictions
 
 from app.config import STATIC_DIR
 
+# TODO: TEST for CUDA if available
+# TODO:  Consider deleting 'with torch.no_grad()' if using model for later explainability methods that require gradients
 class NeuralNetwork:
     img_tensor = []
-    def __init__(self):
-        # -------------------------------------------------------------------
-        # 1. Load environment variables
-        # -------------------------------------------------------------------
-        load_dotenv()
-
-        # -------------------------------------------------------------------
-        # 2. Load pre-trained weights and initialize the model
-        # -------------------------------------------------------------------
-
-        # ImageNet1K V1 is the data set used for training the model and producing the weights
-        # It contains 1k classes, 50k validation images and about 1.2 million training images
-        # The ImageNet-1K dataset is also known as ILSVRC-2012 is maintained by the ImageNet project, of the Stanford Vision and Princeton University (https://www.image-net.org)
-        # It's also available on Kaggle (https://www.kaggle.com/c/imagenet-object-localization-challenge/overview/description)
-        MODEL_SIZE = os.getenv("MODEL_SIZE")
-        print(f"Using model size: {MODEL_SIZE}")
-
-        if MODEL_SIZE is None:
-            raise ValueError("MODEL_SIZE environment variable is not set.")
-        if MODEL_SIZE == "small":
-            self.weights = EfficientNet_V2_S_Weights.IMAGENET1K_V1
-            self.model = efficientnet_v2_s(weights=self.weights)
-        elif MODEL_SIZE == "large":
-            self.weights = EfficientNet_V2_L_Weights.IMAGENET1K_V1
-            self.model = efficientnet_v2_l(weights=self.weights)
-        else:
-            raise ValueError(f"MODEL_SIZE environment variable must be either 'small' or 'large'. Found: {MODEL_SIZE}")
+    
+    def __init__(self):        
+        # using large model
+        self.weights = EfficientNet_V2_L_Weights.IMAGENET1K_V1
+        self.model = efficientnet_v2_l(weights=self.weights)
 
     
     def classify(self, image_url:str) -> Predictions:
-    
-        # -------------------------------------------------------------------
-        # 3. Set the model to evaluation mode and move to device
-        # -------------------------------------------------------------------
-        # TODO: TEST for CUDA if available
-
+        load_dotenv()
         DEVICE = os.getenv("DEVICE")
         print(f"Using device: {DEVICE}")
 
@@ -67,6 +42,7 @@ class NeuralNetwork:
         
         preprocess = self.weights.transforms()
         img = Image.open("src/app/views" + image_url).convert("RGB")
+        
         # Unsqueeze to add a new single-item batch dimension
         # Before: [channels, height, width] = (3, H, W)
         # After: [batch_size, channels, height, width] = (1, 3, H, W)
@@ -75,10 +51,8 @@ class NeuralNetwork:
         self.img_tensor = preprocess(img).unsqueeze(0).to(torch_device)
         self.img_tensor.requires_grad_()
 
-        # -------------------------------------------------------------------
-        # 5. Run inference
-        # -------------------------------------------------------------------
 
+        # TODO:  Consider deleting 'with torch.no_grad()' if using model for later explainability methods that require gradients
         # Normally, PyTorch automatically tracks every operation on tensors so it can later compute gradients for training (via backpropagation).
         # That tracking uses extra memory and slows things down — but it’s only needed during training.
         with torch.no_grad():
